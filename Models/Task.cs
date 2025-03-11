8599f4fd21a72dee84566b6fb523150923a7dde4
ajay-stha <ajay.shrestha54@gmail.com>
2025-03-11 23:49:33 +0545

    Add TaskStatusEnum and update TaskModel
    Introduced a new namespace `TaskManagementSystem.Enum` with the `TaskStatusEnum` enumeration, defining task statuses: `Pending`, `InProgress`, `Completed`, and `Failed`. Updated the `TaskModel` class to include a `Status` property of type `TaskStatusEnum`, initialized to `Pending`, enabling task state management.

---
+++ Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using TaskManagementSystem.Enum;
+
+namespace TaskManagementSystem.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime ExecutionDateTime { get; set; }
+        public TaskStatusEnum Status { get; set; } = TaskStatusEnum.Pending;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
