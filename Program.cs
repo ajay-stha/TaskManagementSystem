76c4dbfbbc15d0ac722b09687d22c6f65bb0ba27
Ajay Shrestha <57993026+ajay-stha@users.noreply.github.com>
2025-03-11 23:46:54 +0545

    Feat/database connection (#3)
    * feat: add more required packages
    
    * feat: add connection string for postgresql datavase
    
    * Add TaskDbContext for TaskModel management
    
    Introduces a new `TaskDbContext` class inheriting from `DbContext` to manage `TaskModel` entities. Includes a `DbSet<TaskModel>` for CRUD operations and a constructor for configuration. The `OnModelCreating` method is overridden for future customization.
    
    * Add Swagger and EF Core with PostgreSQL support
    
    This commit introduces Swagger for API documentation by adding
    `AddEndpointsApiExplorer()` and `AddSwaggerGen()` to the
    service collection. It also configures Entity Framework to
    use PostgreSQL by adding a DbContext for `TaskDbContext`
    and setting up the connection string from the application's
    configuration.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<TaskDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
